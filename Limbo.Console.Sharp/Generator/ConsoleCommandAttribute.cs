4855e7a45b33671d927182813d0a4a6ae6783aa4
Alex Hall <alex@alexhall.org>
2025-05-25 13:52:09 -0500

    Untested. Add basics

---
+++ Limbo.Console.Sharp/Generator/ConsoleCommandAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace Limbo.Console.Sharp;
+
+using System;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class ConsoleCommandAttribute(string name, string description) : Attribute {
+  public string Name { get; } = name;
+  public string Description { get; } = description;
+  public ConsoleCommandAttribute() : this(string.Empty, string.Empty) {}
+  public ConsoleCommandAttribute(string name) : this(name, string.Empty) {}
+}
